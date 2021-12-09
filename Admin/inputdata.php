af7cb410c60fa8ea7f30bc0c215c0005d682aedd
Kerta24 <kertahendrawan@gmail.com>
2021-12-09 12:01:10 +0800

    Menambahkan beberapa fitur admin

--- Admin/inputdata.php
+++ Admin/inputdata.php
@@ -10,14 +10,14 @@
     <title>ADMIN TABLE</title>
 
     <!-- Custom fonts for this template -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
 
     <!-- Custom styles for this template -->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet" />
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet" />
 
     <!-- Custom styles for this page -->
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" />
+    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" />
   </head>
 
   <body id="page-top">
@@ -35,17 +35,12 @@
 
         <!-- Nav Item - Tables -->
         <li class="nav-item">
-          <a class="nav-link" href="tables.html">
+          <a class="nav-link" href="admin.php">
             <i class="fas fa-fw fa-table"></i>
-            <span>Tabel Data</span></a
+            <span>Dashboard</span></a
           >
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="input data.html">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tambah Data</span></a
-          >
-        </li>
+        
       </ul>
       <!-- End of Sidebar -->
 
@@ -161,20 +156,20 @@
     <!-- End of Page Wrapper -->
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="../js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="js/demo/datatables-demo.js"></script>
+    <script src="../js/demo/datatables-demo.js"></script>
   </body>
 </html>
